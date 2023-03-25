b79e81b145813cdf3dcf2ff8e0fa2ab648bbb165
Recep <recep.yesil@payolog.com>
2023-03-25 12:07:29 +0300

    base

---
+++ FordApi/FordApi.Base/Dto/BaseDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FordApi.Base;
+
+public abstract class BaseDto
+{
+    public int Id { get; set; }
+
+
+    [Display(Name = "Created At")]
+    public DateTime CreatedAt { get; set; }
+
+
+    [MaxLength(500)]
+    [Display(Name = "Created By")]
+    public string CreatedBy { get; set; }
+}
